f6921b4ba801d02f7b30eb0fdd366a5e961518c3
vishal <vishrrajput02@gmail.com>
2024-09-27 17:03:37 +0530

    Added the booking model with the basic booking service setup

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+    BUSINESS: 'business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    FIRST_CLASS: 'first-class'
+}
+
+const BOOKING_STATUS = {
+    PENDING: 'Pending',
+    BOOKED: 'Booked',
+    CANCELLED: 'Cancelled',
+    INITIATED: 'Initiated'
+}
+
+module.exports = {
+    SEAT_TYPE,
+    BOOKING_STATUS
+}
