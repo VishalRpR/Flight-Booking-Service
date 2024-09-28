1d154461e94babb7b86ae0dd13aad8e1e20aeadc
vishal <vishrrajput02@gmail.com>
2024-09-28 17:23:18 +0530

    Started Implementing Booking and made the axios call to flight service

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,35 @@
-module.exports={
+const { StatusCodes } = require("http-status-codes");
+const { BookingService } = require("../services")
+const { SuccessResponse, ErrorResponse } = require("../utils/common")
+
+async function createBooking(req,res){
+
+try {
+    console.log("body",req.body);
+    const result=await BookingService.createBooking({
+        flightId:req.body.flightId,
+        userId:req.body.userId,
+        noofSeats:req.body.noofSeats
+    });
+   SuccessResponse.data=result;
+   return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse)
+           
     
+} catch (error) {
+    console.log("controller catching")
+    ErrorResponse.error = error;
+    return res
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json(ErrorResponse);
+
+    
+}
+   
+}
+
+
+module.exports={
+    createBooking
 }
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,28 @@
+const { default: axios } = require("axios")
+const db = require("../models")
+const { ServerConfig } = require("../config")
+
+const AppError = require('../utils/errors/app-error');
+async function createBooking(data) {
+    return new Promise((resolve, reject) => {
+        const result = db.sequelize.transaction(async function bookingImpl(t) {
+            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            console.log(typeof flight);
+            const flightData = flight.data.data;
+            if(data.noofSeats > flightData.totalSeats) {
+                reject(new AppError('Not enough seats available', StatusCodes.BAD_REQUEST));
+            }
+            resolve(true);
+        });
+    })
+
+}
+
+
+
+
+
 module.exports={
+    createBooking
     
 }
